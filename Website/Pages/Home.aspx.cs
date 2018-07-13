d99c4f82dbca480fcfd57a40ad094380dcb58ac0
William Gross <william@wgross.net>
2018-07-13 09:34:51 -0400

    Added home-page banner.

--- Website/Pages/Home.aspx.cs
+++ Website/Pages/Home.aspx.cs
@@ -1,7 +1,18 @@
+using EnterpriseWebLibrary;
 using EnterpriseWebLibrary.EnterpriseWebFramework;
+using EwlRealWorld.Library;
 
 namespace EwlRealWorld.Website.Pages {
 	partial class Home: EwfPage {
-		protected override void loadData() {}
+		protected override void loadData() {
+			if( AppTools.User == null )
+				ph.AddControlsReturnThis(
+					new GenericFlowContainer(
+							new GenericFlowContainer( EwfApp.Instance.AppDisplayName.ToComponents() ).ToCollection<FlowComponent>()
+								.Append( new Paragraph( "A place to share your knowledge.".ToComponents() ) )
+								.Materialize(),
+							classes: ElementClasses.Banner ).ToCollection()
+						.GetControls() );
+		}
 	}
 }
