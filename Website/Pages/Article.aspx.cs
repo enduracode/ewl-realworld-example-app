e1591c94a89e941967356205568c7e2bee947d1c
William Gross <william@wgross.net>
2018-07-23 16:41:21 -0400

    Shortened methods in Article page.

--- Website/Pages/Article.aspx.cs
+++ Website/Pages/Article.aspx.cs
@@ -29,6 +29,8 @@ namespace EwlRealWorld.Website.Pages {
 			public override string ResourceName => Article.Title;
 		}
 
+		private CommentsModification commentMod;
+
 		protected override void loadData() {
 			if( AppTools.User != null && info.Article.AuthorId == AppTools.User.UserId )
 				EwfUiStatics.SetPageActions(
@@ -89,42 +91,8 @@ namespace EwlRealWorld.Website.Pages {
 		private IReadOnlyCollection<Control> getCommentControls() {
 			var controls = new List<Control>();
 
-			CommentsModification commentMod = null;
-			var commentRs = new UpdateRegionSet();
-			if( AppTools.User != null ) {
-				commentMod = getCommentMod();
-				FormState.ExecuteWithDataModificationsAndDefaultAction(
-					PostBack.CreateIntermediate(
-							commentRs.ToCollection(),
-							id: "comment",
-							firstModificationMethod: () => {
-								commentMod.CommentId = MainSequence.GetNextValue();
-								commentMod.Execute();
-							} )
-						.ToCollection(),
-					() => controls.Add(
-						new NamingPlaceholder(
-							commentMod.GetBodyTextTextControlFormItem(
-									false,
-									label: Enumerable.Empty<PhrasingComponent>().Materialize(),
-									controlSetup: TextControlSetup.Create( numberOfRows: 3, placeholder: "Write a comment..." ),
-									value: "" )
-								.ToControl()
-								.ToCollection()
-								.Concat( new EwfButton( new StandardButtonStyle( "Post Comment" ) ).ToCollection().GetControls() ),
-							updateRegionSets: commentRs.ToCollection() ) ) );
-			}
-			else
-				controls.AddRange(
-					new Paragraph(
-							new EwfHyperlink(
-									EnterpriseWebLibrary.EnterpriseWebFramework.EwlRealWorld.Website.UserManagement.LogIn.GetInfo( Home.GetInfo().GetUrl() ),
-									new StandardHyperlinkStyle( "Sign in" ) ).ToCollection()
-								.Concat( " or ".ToComponents() )
-								.Append( new EwfHyperlink( Pages.User.GetInfo(), new StandardHyperlinkStyle( "sign up" ) ) )
-								.Concat( " to add comments on this article.".ToComponents() )
-								.Materialize() ).ToCollection()
-						.GetControls() );
+			var createUpdateRegions = new UpdateRegionSet();
+			controls.AddRange( getNewCommentControls( createUpdateRegions ) );
 
 			var usersById = UsersTableRetrieval.GetRows().ToIdDictionary();
 			controls.AddRange(
@@ -133,48 +101,57 @@ namespace EwlRealWorld.Website.Pages {
 							.OrderByDescending( i => i.CommentId )
 							.Select(
 								i => {
-									var rs = new UpdateRegionSet();
-									var author = usersById[ i.AuthorId ];
+									var deleteUpdateRegions = new UpdateRegionSet();
 									return new Paragraph( i.BodyText.ToComponents() ).ToCollection<FlowComponent>()
-										.Append(
-											new GenericFlowContainer(
-												new GenericFlowContainer(
-														new GenericPhrasingContainer(
-																new EwfHyperlink(
-																	Profile.GetInfo( i.AuthorId ),
-																	new ImageHyperlinkStyle(
-																		new ExternalResourceInfo(
-																			author.ProfilePictureUrl.Any() ? author.ProfilePictureUrl : "https://static.productionready.io/images/smiley-cyrus.jpg" ),
-																		"" ) ).ToCollection() ).ToCollection<PhrasingComponent>()
-															.Append(
-																new GenericPhrasingContainer(
-																	new EwfHyperlink( Profile.GetInfo( i.AuthorId ), new StandardHyperlinkStyle( author.Username ) ).ToCollection() ) )
-															.Append( new GenericPhrasingContainer( i.CreationDateAndTime.ToDayMonthYearString( false ).ToComponents() ) )
-															.Materialize() ).ToCollection<FlowComponent>()
-													.Concat(
-														i.AuthorId == AppTools.User?.UserId
-															? new EwfButton(
-																new StandardButtonStyle( "Delete", icon: new ActionComponentIcon( new FontAwesomeIcon( "fa-trash" ) ) ),
-																behavior: new PostBackBehavior(
-																	postBack: PostBack.CreateIntermediate(
-																		rs.ToCollection(),
-																		id: PostBack.GetCompositeId( "delete", i.CommentId.ToString() ),
-																		firstModificationMethod: () =>
-																			CommentsModification.DeleteRows( new CommentsTableEqualityConditions.CommentId( i.CommentId ) ) ) ) ).ToCollection()
-															: Enumerable.Empty<PhrasingComponent>() )
-													.Materialize() ) )
+										.Append( getCommentFooter( i, usersById[ i.AuthorId ], deleteUpdateRegions ) )
 										.Materialize()
-										.ToComponentListItem( i.CommentId.ToString(), removalUpdateRegionSets: rs.ToCollection() );
+										.ToComponentListItem( i.CommentId.ToString(), removalUpdateRegionSets: deleteUpdateRegions.ToCollection() );
 								} ),
 						setup: new ComponentListSetup(
 							classes: ElementClasses.Comment,
 							itemInsertionUpdateRegions: AppTools.User != null
-								                            ? new ItemInsertionUpdateRegion( commentRs.ToCollection(), () => commentMod.CommentId.ToString().ToCollection() )
-									                            .ToCollection()
+								                            ? new ItemInsertionUpdateRegion(
+									                            createUpdateRegions.ToCollection(),
+									                            () => commentMod.CommentId.ToString().ToCollection() ).ToCollection()
 								                            : null ) ).ToCollection()
 					.GetControls() );
 
 			return controls;
+		}
+
+		private IReadOnlyCollection<Control> getNewCommentControls( UpdateRegionSet createUpdateRegions ) {
+			if( AppTools.User == null )
+				return new Paragraph(
+						new EwfHyperlink(
+								EnterpriseWebLibrary.EnterpriseWebFramework.EwlRealWorld.Website.UserManagement.LogIn.GetInfo( Home.GetInfo().GetUrl() ),
+								new StandardHyperlinkStyle( "Sign in" ) ).ToCollection()
+							.Concat( " or ".ToComponents() )
+							.Append( new EwfHyperlink( Pages.User.GetInfo(), new StandardHyperlinkStyle( "sign up" ) ) )
+							.Concat( " to add comments on this article.".ToComponents() )
+							.Materialize() ).ToCollection()
+					.GetControls()
+					.Materialize();
+
+			commentMod = getCommentMod();
+			return FormState.ExecuteWithDataModificationsAndDefaultAction(
+				PostBack.CreateIntermediate(
+						createUpdateRegions.ToCollection(),
+						id: "comment",
+						firstModificationMethod: () => {
+							commentMod.CommentId = MainSequence.GetNextValue();
+							commentMod.Execute();
+						} )
+					.ToCollection(),
+				() => new NamingPlaceholder(
+					commentMod.GetBodyTextTextControlFormItem(
+							false,
+							label: Enumerable.Empty<PhrasingComponent>().Materialize(),
+							controlSetup: TextControlSetup.Create( numberOfRows: 3, placeholder: "Write a comment..." ),
+							value: "" )
+						.ToControl()
+						.ToCollection()
+						.Concat( new EwfButton( new StandardButtonStyle( "Post Comment" ) ).ToCollection().GetControls() ),
+					updateRegionSets: createUpdateRegions.ToCollection() ).ToCollection() );
 		}
 
 		private CommentsModification getCommentMod() {
@@ -184,5 +161,33 @@ namespace EwlRealWorld.Website.Pages {
 			mod.CreationDateAndTime = DateTime.UtcNow;
 			return mod;
 		}
+
+		private FlowComponent getCommentFooter( CommentsTableRetrieval.Row comment, UsersTableRetrieval.Row author, UpdateRegionSet deleteUpdateRegions ) =>
+			new GenericFlowContainer(
+				new GenericFlowContainer(
+						new GenericPhrasingContainer(
+								new EwfHyperlink(
+									Profile.GetInfo( comment.AuthorId ),
+									new ImageHyperlinkStyle(
+										new ExternalResourceInfo(
+											author.ProfilePictureUrl.Any() ? author.ProfilePictureUrl : "https://static.productionready.io/images/smiley-cyrus.jpg" ),
+										"" ) ).ToCollection() ).ToCollection<PhrasingComponent>()
+							.Append(
+								new GenericPhrasingContainer(
+									new EwfHyperlink( Profile.GetInfo( comment.AuthorId ), new StandardHyperlinkStyle( author.Username ) ).ToCollection() ) )
+							.Append( new GenericPhrasingContainer( comment.CreationDateAndTime.ToDayMonthYearString( false ).ToComponents() ) )
+							.Materialize() ).ToCollection<FlowComponent>()
+					.Concat(
+						comment.AuthorId == AppTools.User?.UserId
+							? new EwfButton(
+									new StandardButtonStyle( "Delete", icon: new ActionComponentIcon( new FontAwesomeIcon( "fa-trash" ) ) ),
+									behavior: new PostBackBehavior(
+										postBack: PostBack.CreateIntermediate(
+											deleteUpdateRegions.ToCollection(),
+											id: PostBack.GetCompositeId( "delete", comment.CommentId.ToString() ),
+											firstModificationMethod: () => CommentsModification.DeleteRows( new CommentsTableEqualityConditions.CommentId( comment.CommentId ) ) ) ) )
+								.ToCollection()
+							: Enumerable.Empty<PhrasingComponent>() )
+					.Materialize() );
 	}
 }
